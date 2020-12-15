4bb9fea96d482c0c59999f8f6a1efb6af8d3f58c
Chris Quintanilla <caq.dev@outlook.com>
2020-12-15 15:57:09 -0500

    Removed unneeded imports from BorrowerService tests

--- LMSBorrowerHibernate/src/test/java/com/ss/lms/service/LmsBorrowerServiceTests.java
+++ LMSBorrowerHibernate/src/test/java/com/ss/lms/service/LmsBorrowerServiceTests.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
